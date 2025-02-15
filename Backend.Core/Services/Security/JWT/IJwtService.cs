ea209f2502e400e1b256549e4d58d6a5459586cf
artemkiruhin <temkaunik@gmail.com>
2025-02-15 20:31:25 +0300

    add: jwt service interface

---
+++ Backend.Core/Services/Security/JWT/IJwtService.cs
@@ -0,0 +1,7 @@
+﻿namespace Backend.Core.Services.Security.JWT;
+
+public interface IJwtService
+{
+    public string GenerateToken(Guid userId, byte[] encodedSecretKey, string audience, string issuer, int expiresInHours);
+    public Guid GetUserId(string token);
+}

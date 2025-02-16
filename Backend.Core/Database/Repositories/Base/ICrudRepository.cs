faf98ad0dfe9e8079123063d57c7c64c367dc18c
artemkiruhin <temkaunik@gmail.com>
2025-02-16 11:48:55 +0300

    add: crud repo interface

---
+++ Backend.Core/Database/Repositories/Base/ICrudRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace Backend.Core.Database.Repositories.Base;
+
+public interface ICrudRepository <T> where T : class
+{
+    Task<T> CreateAsync(T entity);
+    Task<T> UpdateAsync(T entity);
+    Task<T> DeleteAsync(T entity);
+    Task<T> GetByIdAsync(Guid id);
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<IEnumerable<T>> GetFilteredAsync(Expression<Func<T, bool>> predicate);
+}

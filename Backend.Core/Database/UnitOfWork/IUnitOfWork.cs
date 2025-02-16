cfccd3b163f2e1362904b3b8297faffe5a2b4f34
artemkiruhin <temkaunik@gmail.com>
2025-02-16 12:37:34 +0300

    add: uow interface created

---
+++ Backend.Core/Database/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using Backend.Core.Database.Repositories.Interfaces;
+
+namespace Backend.Core.Database.UnitOfWork;
+
+public interface IUnitOfWork : IAsyncDisposable, IDisposable
+{
+    IUserRepository UserRepository { get; }
+    IDepartmentRepository DepartmentRepository { get; }
+    ILogRepository LogRepository { get; }
+    ICommentRepository CommentRepository { get; }
+    IPostRepository PostRepository { get; }
+    IReactionRepository ReactionRepository { get; }
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    Task BeginTransactionAsync(CancellationToken cancellationToken = default);
+    Task CommitTransactionAsync(CancellationToken cancellationToken = default);
+    Task RollbackTransactionAsync(CancellationToken cancellationToken = default);
+}

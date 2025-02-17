fd6549ebcc4591645ac24fd95bc28b50357d08a0
artemkiruhin <temkaunik@gmail.com>
2025-02-17 19:26:14 +0300

    add: dep create use-case

--- Backend.Core/UseCases/CreateDepartmentUseCase.cs
+++ Backend.Core/UseCases/CreateDepartmentUseCase.cs
@@ -1,11 +1,39 @@
-﻿using Backend.Core.Models.DTOs.Response;
+﻿using Backend.Core.Database.UnitOfWork;
+using Backend.Core.Models.DTOs.Response;
+using Backend.Core.Models.Entities;
 
 namespace Backend.Core.UseCases;
 
 public class CreateDepartmentUseCase
 {
-    public async Task<Result<Guid>> ExecuteAsync()
+    private readonly IUnitOfWork _database;
+
+    public CreateDepartmentUseCase(IUnitOfWork unitOfWork)
     {
-        throw new NotImplementedException();
+        _database = unitOfWork;
+    }
+    
+    public async Task<Result<Guid>> ExecuteAsync(string name)
+    {
+        try
+        {
+            var departments = await _database.DepartmentRepository.GetByNameAsync(name);
+            var department = departments.FirstOrDefault();
+            if (department != null) return Result<Guid>.Failure($"Отдел с названием: {name} уже существует!");
+            
+            var newDepartment = DepartmentEntity.Create(name);
+
+            await _database.BeginTransactionAsync();
+            await _database.DepartmentRepository.CreateAsync(newDepartment);
+            await _database.SaveChangesAsync();
+            await _database.CommitTransactionAsync();
+            
+            return Result<Guid>.Success(newDepartment.Id);
+        }
+        catch (Exception e)
+        {
+            await _database.RollbackTransactionAsync();
+            return Result<Guid>.Failure("Ошибка создания" + e.Message);
+        }
     }
 }

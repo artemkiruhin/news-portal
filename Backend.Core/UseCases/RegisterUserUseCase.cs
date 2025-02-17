9ebbfe77227b5f26b9c8b6de5993459bc666a62f
artemkiruhin <temkaunik@gmail.com>
2025-02-17 19:15:21 +0300

    add: user registration created

--- Backend.Core/UseCases/RegisterUserUseCase.cs
+++ Backend.Core/UseCases/RegisterUserUseCase.cs
@@ -1,11 +1,40 @@
-﻿using Backend.Core.Models.DTOs.Response;
+﻿using Backend.Core.Database.UnitOfWork;
+using Backend.Core.Models.DTOs.Response;
+using Backend.Core.Models.Entities;
+using Backend.Core.Services.Security.Hash;
 
 namespace Backend.Core.UseCases;
 
 public class RegisterUserUseCase
 {
-    public async Task<Result<Guid>> ExecuteAsync()
+    private readonly IUnitOfWork _database;
+    private readonly IHasher _hasher;
+
+    public RegisterUserUseCase(IUnitOfWork database, IHasher hasher)
     {
-        throw new NotImplementedException();
+        _database = database;
+        _hasher = hasher;
+    }
+    
+    public async Task<Result<Guid>> ExecuteAsync(string username, string password, string? email, Guid departmentId, bool hasPublishedRights = false)
+    {
+        try
+        {
+            var user = await _database.UserRepository.GetByUsernameAsync(username);
+            if (user != null) return Result<Guid>.Failure($"Пользователь с логином {username} уже зарегистрирован!");
+            
+            var department = await _database.DepartmentRepository.GetByIdAsync(departmentId);
+            if (department == null) return Result<Guid>.Failure($"Отдела с id: {departmentId} не существует!");
+            
+            var hashedPassword = _hasher.Hash(password);
+
+            var newUser = UserEntity.Create(username, hashedPassword, email, hasPublishedRights, departmentId);
+            var result = await _database.UserRepository.CreateAsync(newUser);
+            return Result<Guid>.Success(result.Id);
+        }
+        catch (Exception e)
+        {
+            return Result<Guid>.Failure("Ошибка регистрации" + e.Message);
+        }
     }
 }

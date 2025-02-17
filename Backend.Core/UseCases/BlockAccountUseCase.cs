8eb4543f1e1cb23a59bc6dcdb40120f72708b280
artemkiruhin <temkaunik@gmail.com>
2025-02-17 19:20:17 +0300

    add: user block created

--- Backend.Core/UseCases/BlockAccountUseCase.cs
+++ Backend.Core/UseCases/BlockAccountUseCase.cs
@@ -1,11 +1,37 @@
-﻿using Backend.Core.Models.DTOs.Response;
+﻿using Backend.Core.Database.UnitOfWork;
+using Backend.Core.Models.DTOs.Response;
 
 namespace Backend.Core.UseCases;
 
 public class BlockAccountUseCase
 {
-    public async Task<Result<Guid>> ExecuteAsync()
+    private readonly IUnitOfWork _database;
+
+    public BlockAccountUseCase(IUnitOfWork unitOfWork)
     {
-        throw new NotImplementedException();
+        _database = unitOfWork;
+    }
+    
+    public async Task<Result<Guid>> ExecuteAsync(Guid userId)
+    {
+        try
+        {
+            var user = await _database.UserRepository.GetByIdAsync(userId);
+            if (user == null) return Result<Guid>.Failure($"Пользователя с id: {userId} не существует!");
+            
+            await _database.BeginTransactionAsync();
+            
+            user.HasPublishRights = false;
+            await _database.UserRepository.UpdateAsync(user);
+            await _database.SaveChangesAsync();
+            
+            await _database.CommitTransactionAsync();
+            return Result<Guid>.Success(user.Id);
+        }
+        catch (Exception e)
+        {
+            await _database.RollbackTransactionAsync();
+            return Result<Guid>.Failure("Ошибка блокировки" + e.Message);
+        }
     }
 }

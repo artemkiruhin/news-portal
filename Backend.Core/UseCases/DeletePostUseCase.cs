06c34d16b3e1cd69eaf4360f6c14838c6f7ff7ed
artemkiruhin <temkaunik@gmail.com>
2025-02-18 13:33:06 +0300

    add: post deletion

--- Backend.Core/UseCases/DeletePostUseCase.cs
+++ Backend.Core/UseCases/DeletePostUseCase.cs
@@ -1,11 +1,35 @@
-﻿using Backend.Core.Models.DTOs.Response;
+﻿using Backend.Core.Database.UnitOfWork;
+using Backend.Core.Models.DTOs.Response;
 
 namespace Backend.Core.UseCases;
 
 public class DeletePostUseCase
 {
-    public async Task<Result<Guid>> ExecuteAsync()
+    private readonly IUnitOfWork _database;
+
+    public DeletePostUseCase(IUnitOfWork unitOfWork)
     {
-        throw new NotImplementedException();
+        _database = unitOfWork;
+    }
+    
+    public async Task<Result<Guid>> ExecuteAsync(Guid id)
+    {
+        try
+        {
+            var post = await _database.PostRepository.GetByIdAsync(id);
+            if (post == null) return Result<Guid>.Failure($"Новость с id: {id} не найдена!");
+
+            await _database.BeginTransactionAsync();
+            post.IsDeleted = true;
+            await _database.PostRepository.UpdateAsync(post);
+            await _database.SaveChangesAsync();
+            await _database.CommitTransactionAsync();
+            return Result<Guid>.Success(post.Id);
+        }
+        catch (Exception e)
+        {
+            await _database.RollbackTransactionAsync();
+            return Result<Guid>.Failure("Ошибка при удалении новости " + e.Message);
+        }
     }
 }

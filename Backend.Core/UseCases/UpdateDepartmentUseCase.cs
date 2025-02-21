396decc2313ecc6ffb13ce82f8f8f210cbc1c1ae
artemkiruhin <temkaunik@gmail.com>
2025-02-21 21:48:35 +0300

    add: department create use case

--- Backend.Core/UseCases/UpdateDepartmentUseCase.cs
+++ Backend.Core/UseCases/UpdateDepartmentUseCase.cs
@@ -1,11 +1,36 @@
-﻿using Backend.Core.Models.DTOs.Response;
+﻿using Backend.Core.Database.UnitOfWork;
+using Backend.Core.Models.DTOs.Response;
 
 namespace Backend.Core.UseCases;
 
 public class UpdateDepartmentUseCase
 {
-    public async Task<Result<Guid>> ExecuteAsync()
+    private readonly IUnitOfWork _database;
+
+    public UpdateDepartmentUseCase(IUnitOfWork unitOfWork)
     {
-        throw new NotImplementedException();
+        _database = unitOfWork;
+    }
+    
+    public async Task<Result<Guid>> ExecuteAsync(Guid id, string name)
+    {
+        try
+        {
+            var department = await _database.DepartmentRepository.GetByIdAsync(id);
+            if (department == null) return Result<Guid>.Failure($"Отдел с id: {id} не найден!");
+            if (department.Name == name) return Result<Guid>.Failure($"Отдел уже имеет указанное имя!");
+
+            await _database.BeginTransactionAsync();
+            department.Name = name;
+            await _database.DepartmentRepository.UpdateAsync(department);
+            await _database.SaveChangesAsync(); 
+            await _database.CommitTransactionAsync();
+            return Result<Guid>.Success(department.Id);
+        }
+        catch (Exception e)
+        {
+            await _database.RollbackTransactionAsync();
+            return Result<Guid>.Failure("Ошибка изменения" + e.Message);
+        }
     }
 }

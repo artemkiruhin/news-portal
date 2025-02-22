22de194a09caf0ce1a7bc9dddd82207048105f0b
artemkiruhin <temkaunik@gmail.com>
2025-02-22 12:54:35 +0300

    add: update comment use case

--- Backend.Core/UseCases/UpdateCommentUseCase.cs
+++ Backend.Core/UseCases/UpdateCommentUseCase.cs
@@ -1,11 +1,37 @@
-﻿using Backend.Core.Models.DTOs.Response;
+﻿using Backend.Core.Database.UnitOfWork;
+using Backend.Core.Models.DTOs.Response;
 
 namespace Backend.Core.UseCases;
 
 public class UpdateCommentUseCase
 {
-    public async Task<Result<Guid>> ExecuteAsync()
+    private readonly IUnitOfWork _database;
+
+    public UpdateCommentUseCase(IUnitOfWork unitOfWork)
     {
-        throw new NotImplementedException();
+        _database = unitOfWork;
+    }
+    
+    public async Task<Result<Guid>> ExecuteAsync(Guid commentId, string content)
+    {
+        try
+        {
+            var comment = await _database.CommentRepository.GetByIdAsync(commentId);
+            if (comment == null) return Result<Guid>.Failure($"Комментарий с id: {commentId} не найден!");
+            
+            await _database.BeginTransactionAsync();
+            
+            comment.Content = content;
+            await _database.CommentRepository.UpdateAsync(comment);
+            await _database.SaveChangesAsync();
+            await _database.CommitTransactionAsync();
+            
+            return Result<Guid>.Success(comment.Id);
+        }
+        catch (Exception e)
+        {
+            await _database.RollbackTransactionAsync();
+            return Result<Guid>.Failure("Ошибка изменения: " + e.Message);
+        }
     }
 }

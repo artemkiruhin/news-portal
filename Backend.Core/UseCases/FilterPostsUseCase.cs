563e9a6c1f0318bbef21a29480dd4a03d1ebd6f4
artemkiruhin <temkaunik@gmail.com>
2025-02-22 13:17:11 +0300

    add: filter posts use case

--- Backend.Core/UseCases/FilterPostsUseCase.cs
+++ Backend.Core/UseCases/FilterPostsUseCase.cs
@@ -1,11 +1,75 @@
-﻿using Backend.Core.Models.DTOs.Response;
+﻿using Backend.Core.Database.UnitOfWork;
+using Backend.Core.Models.DTOs.Response;
+using Backend.Core.Models.Entities;
 
 namespace Backend.Core.UseCases;
 
 public class FilterPostsUseCase
 {
-    public async Task<Result<List<PostResponse>>> ExecuteAsync()
+    private readonly IUnitOfWork _database;
+
+    public FilterPostsUseCase(IUnitOfWork unitOfWork)
     {
-        throw new NotImplementedException();
+        _database = unitOfWork;
+    }
+    
+    public async Task<Result<List<PostResponse>>> ExecuteAsync(string? fullContent, Guid? publisherId, List<Guid>? departmentIds, DateTime? startDate, DateTime? endDate)
+    {
+        try
+        {
+            if (fullContent is null && !publisherId.HasValue && departmentIds == null && startDate == null && endDate == null)
+                return Result<List<PostResponse>>.Success(new List<PostResponse>());
+
+            var departments = new List<DepartmentEntity>();
+            if (departmentIds != null && departmentIds.Count != 0)
+            {
+                foreach (var depId in departmentIds)
+                {
+                    var department = await _database.DepartmentRepository.GetByIdAsync(depId);
+                    if (department == null)
+                        return Result<List<PostResponse>>.Failure($"Отдел с id: {depId} не найден!");
+                    departments.Add(department);
+                }
+            }
+            
+            var posts = await _database.PostRepository.GetFilteredAsync(post =>
+                (!string.IsNullOrEmpty(fullContent) && 
+                    (post.Title.Contains(fullContent) || 
+                    (!string.IsNullOrEmpty(post.Subtitle) && post.Subtitle.Contains(fullContent)) || 
+                    post.Content.Contains(fullContent))) ||
+                (publisherId.HasValue && post.Publisher.Id == publisherId.Value) ||
+                (startDate.HasValue && post.PublishedAt >= startDate.Value) ||
+                (endDate.HasValue && post.PublishedAt <= endDate.Value) ||
+                (departments.Any() && post.Departments.Any(d => departments.Contains(d)))
+            );
+
+            if (posts == null || !posts.Any())
+                return Result<List<PostResponse>>.Success(new List<PostResponse>());
+
+
+
+            var postResponses = posts.Select(post => new PostResponse
+            {
+                Id = post.Id,
+                Title = post.Title,
+                Subtitle = post.Subtitle,
+                ContentPreview = post.Content,
+                AuthorUsername = post.Publisher.Username,
+                PublishedAt = post.PublishedAt,
+                LastModifiedAt = post.LastModifiedAt,
+                Reactions = new ReactionStatsResponse(
+                    post.Reactions.Count(x => x.Type == ReactionType.Like),
+                    post.Reactions.Count(x => x.Type == ReactionType.Dislike),
+                    post.Reactions.Count(x => x.Type == ReactionType.Checked)
+                )
+            }).ToList();
+
+            return Result<List<PostResponse>>.Success(postResponses);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return Result<List<PostResponse>>.Failure($"Произошла ошибка при фильтрации постов: {e.Message}");
+        }
     }
 }

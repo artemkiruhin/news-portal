a901cf4482e862fa28932273aad634958ed7c817
artemkiruhin <temkaunik@gmail.com>
2025-02-23 12:13:25 +0300

    add: reactions api

---
+++ Backend.Api/Controllers/ReactionsController.cs
@@ -0,0 +1,68 @@
+using Backend.Core.Database.UnitOfWork;
+using Backend.Core.Models.Entities;
+using Backend.Core.UseCases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReactionsController : ControllerBase
+    {
+        private readonly ReactionStatsByPostUseCase _reactionStatsByPostUseCase;
+        private readonly ReactPostUseCase _reactionPostUseCase;
+        private readonly IUnitOfWork _database;
+
+        public ReactionsController(ReactionStatsByPostUseCase reactionStatsByPostUseCase,
+            ReactPostUseCase reactionPostUseCase, IUnitOfWork database)
+        {
+            _reactionStatsByPostUseCase = reactionStatsByPostUseCase;
+            _reactionPostUseCase = reactionPostUseCase;
+            _database = database;
+        }
+
+        [HttpGet("post/{postId:guid}")]
+        public async Task<IActionResult> GetAllByPostId(Guid postId, CancellationToken ct)
+        {
+            try
+            {
+                var result = await _reactionStatsByPostUseCase.ExecuteAsync(postId, ct);
+                return result.IsSuccess ? Ok(result.Value) : BadRequest(result.ErrorMessage);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        
+        [HttpPost("create")]
+        public async Task<IActionResult> Create(Guid postId, Guid senderId, ReactionType type, CancellationToken ct)
+        {
+            try
+            {
+                var result = await _reactionPostUseCase.ExecuteAsync(postId, senderId, type, ct);
+                return result.IsSuccess ? Ok(result.Value) : BadRequest(result.ErrorMessage);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        
+        [HttpDelete("delete/{id:guid}")]
+        public async Task<IActionResult> Delete(Guid id, CancellationToken ct)
+        {
+            try
+            {
+                var react = await _database.ReactionRepository.GetByIdAsync(id, ct);
+                if (react == null) return NotFound();
+                var result = await _database.ReactionRepository.DeleteAsync(react, ct);
+                return Ok(result.Id);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}

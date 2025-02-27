18ec3c72fea3d62cf1e83a757b6c409c0cd985a7
artemkiruhin <temkaunik@gmail.com>
2025-02-27 21:37:25 +0300

    upd: app.js added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NewsPage from "./pages/NewsPage/NewsPage";
+import NewsDetailPage from "./pages/NewsDetailsPage/NewsDetailPage";
+import NewsEditPage from "./pages/NewsEditPage/NewsEditPage";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+      <Router>
+        <div className="app">
+          <Routes>
+            <Route path="/" element={<NewsPage />} />
+            <Route path="/news/:id" element={<NewsDetailPage />} />
+            <Route path="/news/edit/:id" element={<NewsEditPage />} />
+          </Routes>
+        </div>
+      </Router>
   );
 }
 

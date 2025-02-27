2edcd82907680066a7943389abfc8939feb48582
artemkiruhin <temkaunik@gmail.com>
2025-02-27 21:27:40 +0300

    add: navbar

--- frontend/src/components/Layouts/Container/Container.jsx
+++ frontend/src/components/Layouts/Container/Container.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from "../Navbar/Navbar";
 
 const Container = ({ children }) => {
     return (
         <div className="layout">
-            {/*<Navbar />*/}
+            <Navbar />
             <main className="container">
                 {children}
             </main>
---
+++ frontend/src/components/Layouts/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="container navbar-container">
+                <h1 className="nav-logo">АЛЬФА</h1>
+                <ul className="nav-list">
+                    <li className="nav-list-element">
+                        <a className="nav-link">Главная</a>
+                    </li>
+                    <li className="nav-list-element">
+                        <a className="nav-link">О нас</a>
+                    </li>
+                    <li className="nav-list-element">
+                        <a className="nav-link nav-link-logout">Выйти</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;

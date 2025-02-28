578b4446dda252f91dae2bbee84163e1398e4a04
artemkiruhin <temkaunik@gmail.com>
2025-02-28 18:25:28 +0300

    fix: styles refs

--- frontend/src/pages/NewsEditPage/NewsEditPage.jsx
+++ frontend/src/pages/NewsEditPage/NewsEditPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import '../styles/newsEditor.css';
+import './NewsEditor.css';
 import ReactMarkdown from 'react-markdown';
 
 import {

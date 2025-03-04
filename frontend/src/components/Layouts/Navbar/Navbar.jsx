7eae34a8c553487970744f52c6cf557d36c130ed
artemkiruhin <temkaunik@gmail.com>
2025-03-04 16:23:03 +0300

    add: navbar logo nav

--- frontend/src/components/Layouts/Navbar/Navbar.jsx
+++ frontend/src/components/Layouts/Navbar/Navbar.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import "./Navbar.css"
+import {useNavigate} from "react-router-dom";
 
 const Navbar = () => {
+
+    const navigate = useNavigate();
+
     return (
         <nav className="navbar">
             <div className="container navbar-container">
-                <h1 className="nav-logo">АЛЬФА</h1>
+                <h1 className="nav-logo" onClick={() => navigate("/")} >АЛЬФА</h1>
                 <ul className="nav-list">
                     <li className="nav-list-element">
                         <a className="nav-link">Главная</a>

482607e27488cb38abacbbcc6397ab87043f0127
artemkiruhin <temkaunik@gmail.com>
2025-03-05 11:27:48 +0300

    add: auth-handler

---
+++ frontend/src/api-handlers/auth-handler.js
@@ -0,0 +1,60 @@
+import {BASE_URL} from "./base-handler";
+
+const login = async (username, password) => {
+    try {
+        const response = await fetch(`${BASE_URL}/login`, {
+            method: 'POST',
+            body: JSON.stringify({
+                username: username,
+                password: password
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include'
+        })
+
+        if (!response.ok) {
+            console.error(`Ошибка: ${response.statusText} | ${response.status}`)
+        }
+
+        const data = await response.json()
+
+        return data.token
+
+    } catch (e) {
+        console.error("Ошибка авторизации: ", e)
+    }
+}
+
+const register = async (username, password) => {
+    try {
+        const response = await fetch(`${BASE_URL}/register`, {
+            method: 'POST',
+            body: JSON.stringify({
+                username: username,
+                password: password
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include'
+        })
+
+        if (!response.ok) {
+            console.error(`Ошибка: ${response.statusText} | ${response.status}`)
+        }
+
+        const data = await response.json()
+
+        return data.token
+
+    } catch (e) {
+        console.error("Ошибка регистрации: ", e)
+    }
+}
+
+export {
+    login,
+    register
+}
